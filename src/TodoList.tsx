841d107740f5ff31bf434cc46a17343bf4281d84
Wes Bosman <bosmanwn@vcu.edu>
2023-05-05 07:11:00 -0400

    Add blank line under todo list items and strike through an item when its complete

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -6,7 +6,7 @@ const TodoList = () => {
     const date = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"}) 
 
     return (
-        <div className="flex flex-col items-center shadow-lg bg-slate-50" style={{
+        <div className="flex flex-col items-center shadow-lg bg-slate-50 shadow-md pb-2" style={{
                 background: 'linear-gradient(0.25turn, white 24px, rgb(248 113 113) 2px, rgb(248 113 113) 2px, white 26px, white)',
             }}>
             <div className="mt-8 w-full" >
@@ -14,6 +14,9 @@ const TodoList = () => {
                 {rootStore.items.map((item: any, i: any) => {
                     return (<TodoItem key={i} item={item}/>);
                 })}
+            </div>
+            <div className="mt-8 w-full">
+                <div className="flex text-slate-700 w-full pl-8 gap-2 border-t-2 border-blue-400">&nbsp;</div>
             </div>
         </div>
     )
