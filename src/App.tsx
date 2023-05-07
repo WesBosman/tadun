9607b2538c3fec7880c9e82ce3fe5edde51c3f42
Wes Bosman <bosmanwn@vcu.edu>
2023-05-07 07:54:32 -0400

    Remove div from around the input field

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,21 @@ import TodoList from './TodoList'
 
 function App() {
   return (
-    <div className="max-w-sm p-4 m-auto bg-slate-800">
+    <div className="flex flex-col gap-4 max-w-lg p-4 m-auto bg-slate-800">
       <h1 className="text-slate-100">Tadun</h1>
-      <div className="mb-2 p-2 rounded-xl shadow-lg bg-slate-400">
-        <input className="px-3 py-1 w-full rounded" type="text" placeholder="Add item" onKeyDown={(e) => {
-          if (e.target.value === '') {
-            return;
-          }
-          if (e.key === 'Enter') {
-            rootStore.addTodo(TodoItem.create({
-              id: 2,
-              title: e.target.value,
-              isCompleted: false,
-            }));
-            e.target.value = '';
-          }
-        }}/>
-      </div>
+      <input className="px-3 py-1 w-full rounded" type="text" placeholder="Add item" onKeyDown={(e) => {
+        if (e.target.value === '') {
+          return;
+        }
+        if (e.key === 'Enter') {
+          rootStore.addTodo(TodoItem.create({
+            id: 2,
+            title: e.target.value,
+            isCompleted: false,
+          }));
+          e.target.value = '';
+        }
+      }}/>
       <TodoList/>
     </div>
   )
