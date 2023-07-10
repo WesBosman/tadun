5d2554726e95e7de1c5ef9037569a1b2d51f2d1d
Wes Bosman <wes@nudgecoach.com>
2023-07-10 14:04:23 -0400

    Update widths to use percentage based units.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import TodoList from './TodoList'
 
 function App() {
   return (
-    <div className="flex flex-col gap-4 max-w-lg p-4 m-auto bg-slate-800">
+    <div className="flex flex-col gap-4 w-3/5 p-4 m-auto bg-slate-800">
       <h1 className="text-slate-100">Tadun</h1>
       <input className="px-3 py-1 w-full rounded" type="text" placeholder="Add item" onKeyDown={(e) => {
         if (e.target.value === '') {
--- src/TodoItem.tsx
+++ src/TodoItem.tsx
@@ -10,7 +10,7 @@ const TodoItem: FC<TodoProps> = ({item}): JSX.Element => {
     const isCompletedClass = item.isCompleted ? 'line-through' : '';
     return (
         <span className={"flex w-full"}>
-            <label htmlFor="item" className={`flex justify-between w-96 text-slate-700 pl-8 pr-1 gap-2 hover:cursor-pointer`} onClick={() => {
+            <label htmlFor="item" className={`flex justify-between w-4/5 text-slate-700 pl-8 pr-1 gap-2 hover:cursor-pointer`} onClick={() => {
                 item.setIsCompleted()
             }}>
                 <div className={"flex gap-2"}>
@@ -19,7 +19,7 @@ const TodoItem: FC<TodoProps> = ({item}): JSX.Element => {
                 </div>
                 <span>{item.date}</span>
             </label>
-            <span className={"tooltip hover:cursor-pointer hover:text-blue-700 px-2 text-blue-500"} onClick={() => item.setShowActions()}>Actions
+            <span className={"tooltip hover:cursor-pointer hover:text-blue-700 w-1/5 px-2 text-blue-500"} onClick={() => item.setShowActions()}>Actions
                 <div className={`${item.showActionClass} tooltip-text flex align-center justify-around gap-2 w-30 bg-gray-100 rounded`}>
                     <div className={"hover:text-red-700 text-red-500 px-2"} onClick={item.delete}>Delete</div>
                 </div>
